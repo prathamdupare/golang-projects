0ee6b9002d655e88b827455d6e73800ca1d3c363
prathamdupare <prathamdupare@proton.me>
2024-06-11 15:20:09 +0530

    added create operation for movie

--- CRUD-API/main.go
+++ CRUD-API/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -40,6 +42,15 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
@@ -60,7 +71,7 @@ func main() {
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	//	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	//	r.HandleFunc("/movies/createMovie", createMovie).Methods("POST")
+	r.HandleFunc("/movies/createMovie", createMovie).Methods("POST")
 	//	r.Handle("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
