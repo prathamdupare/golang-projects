aea70fed6b428b3e06fd95c4941c744ef08ec02d
prathamdupare <prathamdupare@proton.me>
2024-06-11 15:14:09 +0530

    added get and delete operation

--- CRUD-API/main.go
+++ CRUD-API/main.go
@@ -28,6 +28,30 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -38,7 +62,7 @@ func main() {
 	//	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	//	r.HandleFunc("/movies/createMovie", createMovie).Methods("POST")
 	//	r.Handle("/movies/{id}", updateMovie).Methods("PUT")
-	//	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("starting server at port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
