198e66974769890d2c6204ebac3dc651b28d0d2a
prathamdupare <prathamdupare@proton.me>
2024-06-11 15:38:18 +0530

    added delete and update movie funcs

--- CRUD-API/main.go
+++ CRUD-API/main.go
@@ -63,6 +63,24 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -70,9 +88,9 @@ func main() {
 	movies = append(movies, Movie{ID: "2", Isbn: "12345", Title: "TWO", Director: &Director{Firstname: "Jane", Lastname: "Doe"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	//	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies/createMovie", createMovie).Methods("POST")
-	//	r.Handle("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("starting server at port 8000")
